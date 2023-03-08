aa3c47b17e0e6cb296ee1603cd633306e2fba9da
Axe <ngsmo@iscte-iul.pt>
2023-03-08 19:36:19 +0000

    some tests

---
+++ src/MyLibrary.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import  { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/*
+    vm.expectRevert catch errors on the next call. CALL = 1
+    If we increment the call number, we don't catch the error. CALL = 1 + 1
+*/
+
+library MyLibrary {
+
+    error getBalanceAndRevertError();
+    error justRevertError();
+
+    function getBalanceAndRevert(IERC20 token, address receiver) internal
+    {
+        // this will make expectRevert to be call + 1 :(
+        uint256 myCrazyBalance = token.balanceOf(receiver);
+        revert getBalanceAndRevertError();
+    }
+
+    function getRevertAndBalance(IERC20 token, address receiver) internal
+    {
+        // this will make expectRevert call = 1
+        revert getBalanceAndRevertError();
+        uint256 myCrazyBalance = token.balanceOf(receiver);
+    }
+
+    function justRevert(IERC20 token) internal
+    {
+        // this will make expectRevert call = 1
+        revert justRevertError();
+    }
+}
---
+++ test/Library.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/console.sol";
+import "forge-std/Test.sol";
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { MyLibrary } from "../src/MyLibrary.sol";
+
+contract LibraryTest is Test {
+
+
+	address internal constant alice = address(0x421);
+
+	using MyLibrary for ERC20;
+
+	ERC20 token;
+
+	constructor () { }
+
+	function setUp() public {
+		token = new ERC20("TestToken", "TT");
+	}
+
+	function testCallWillRevert() public {
+		vm.startPrank(alice);
+		vm.expectRevert(MyLibrary.justRevertError.selector);
+		token.justRevert();
+		vm.stopPrank();
+	}
+
+	function testCallShouldRevertAfterExternalCall() public {
+		vm.startPrank(alice);
+		vm.expectRevert(MyLibrary.getBalanceAndRevertError.selector);
+		token.getBalanceAndRevert(alice);
+		vm.stopPrank();
+	}
+
+	function testCallShouldRevertBeforeExternalCall() public {
+		vm.startPrank(alice);
+		vm.expectRevert(MyLibrary.getBalanceAndRevertError.selector);
+		token.getRevertAndBalance(alice);
+		vm.stopPrank();
+	}
+}
